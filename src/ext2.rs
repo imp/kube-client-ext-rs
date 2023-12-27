7e793bbf1c8d3f8801d64dc387917e526618d41c
Cyril Plisko <cyril.plisko@mountall.com>
2023-12-27 09:53:37 +0200

    Separate get_xxx and get_xxx_opt methods

--- src/ext2.rs
+++ src/ext2.rs
@@ -7,13 +7,35 @@ use super::*;
 #[async_trait::async_trait(?Send)]
 pub trait KubeClientExt2: KubeClientExt {
     /// Get named secret from a given (or default) namespace
+    /// Return `None` if not found`
     ///
-    async fn get_secret(
+    async fn get_secret_opt(
         &self,
         name: &str,
         namespace: impl Into<Option<&str>>,
     ) -> client::Result<Option<corev1::Secret>> {
         self.secrets(namespace).get_opt(name).await
+    }
+
+    /// Get named secret from a given (or default) namespace
+    ///
+    async fn get_secret(
+        &self,
+        name: &str,
+        namespace: impl Into<Option<&str>>,
+    ) -> client::Result<corev1::Secret> {
+        self.secrets(namespace).get(name).await
+    }
+
+    /// Get named deployment from a given (or default) namespace
+    /// Return `None` if not found
+    ///
+    async fn get_deployment_opt(
+        &self,
+        name: &str,
+        namespace: impl Into<Option<&str>>,
+    ) -> client::Result<Option<appsv1::Deployment>> {
+        self.deployments(namespace).get_opt(name).await
     }
 
     /// Get named deployment from a given (or default) namespace
@@ -22,17 +44,34 @@ pub trait KubeClientExt2: KubeClientExt {
         &self,
         name: &str,
         namespace: impl Into<Option<&str>>,
-    ) -> client::Result<Option<appsv1::Deployment>> {
-        self.deployments(namespace).get_opt(name).await
+    ) -> client::Result<appsv1::Deployment> {
+        self.deployments(namespace).get(name).await
     }
 
     /// Get named api service
+    /// Return `None` if not found
     ///
-    async fn get_apiservice(
+    async fn get_apiservice_opt(
         &self,
         name: &str,
     ) -> client::Result<Option<apiregistrationv1::APIService>> {
         self.apiservices().get_opt(name).await
+    }
+
+    /// Get named api service
+    ///
+    async fn get_apiservice(&self, name: &str) -> client::Result<apiregistrationv1::APIService> {
+        self.apiservices().get(name).await
+    }
+
+    /// Get named CRD
+    /// Return `None` if not found
+    ///
+    async fn get_crd_opt(
+        &self,
+        name: &str,
+    ) -> client::Result<Option<apiextensionsv1::CustomResourceDefinition>> {
+        self.crds().get_opt(name).await
     }
 
     /// Get named CRD
@@ -40,8 +79,8 @@ pub trait KubeClientExt2: KubeClientExt {
     async fn get_crd(
         &self,
         name: &str,
-    ) -> client::Result<Option<apiextensionsv1::CustomResourceDefinition>> {
-        self.crds().get_opt(name).await
+    ) -> client::Result<apiextensionsv1::CustomResourceDefinition> {
+        self.crds().get(name).await
     }
 
     /// Get owner object from `ownerReference` assuming it is of kind `K`
@@ -68,6 +107,41 @@ pub trait KubeClientExt2: KubeClientExt {
         } else {
             Ok(None)
         }
+    }
+
+    /// List all `Pod`s  in a given (or default) namespace
+    ///
+    async fn list_pods(
+        &self,
+        namespace: impl Into<Option<&str>>,
+    ) -> client::Result<Vec<corev1::Pod>> {
+        self.list_k(namespace).await
+    }
+
+    /// List all `Deployment`s in a given (or default) namespace
+    ///
+    async fn list_deployments(
+        &self,
+        namespace: impl Into<Option<&str>>,
+    ) -> client::Result<Vec<appsv1::Deployment>> {
+        self.list_k(namespace).await
+    }
+
+    /// List namespaced objects of kind `K` in a given (or default) namespace
+    ///
+    async fn list_k<K>(&self, namespace: impl Into<Option<&str>>) -> client::Result<Vec<K>>
+    where
+        K: Clone
+            + fmt::Debug
+            + k8s::openapi::serde::de::DeserializeOwned
+            + client::Resource<Scope = k8s::openapi::NamespaceResourceScope>,
+        <K as client::Resource>::DynamicType: Default,
+    {
+        let lp = self.list_params();
+        self.namespaced_k(namespace)
+            .list(&lp)
+            .await
+            .map(|list| list.items)
     }
 }
 
