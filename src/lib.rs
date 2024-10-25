6bfc65df8df21061f1532da815a0b5a75d38d97e
Cyril Plisko <cyril.plisko@mountall.com>
2024-10-25 15:15:29 +0300

    Add more clippy lints

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,20 @@
+#![cfg_attr(feature = "pedantic", warn(clippy::pedantic))]
+#![warn(clippy::use_self)]
+#![warn(clippy::map_flatten)]
+#![warn(clippy::map_unwrap_or)]
+#![warn(deprecated_in_future)]
+#![warn(future_incompatible)]
+#![warn(noop_method_call)]
+#![warn(unreachable_pub)]
+#![warn(missing_debug_implementations)]
+#![warn(rust_2018_compatibility)]
+#![warn(rust_2021_compatibility)]
+// #![warn(rust_2024_compatibility)]
+#![warn(rust_2024_incompatible_pat)]
+#![warn(rust_2018_idioms)]
+#![warn(unused)]
+#![deny(warnings)]
+
 use k8s_openapi_ext as k8s;
 
 use k8s::apiextensionsv1;
